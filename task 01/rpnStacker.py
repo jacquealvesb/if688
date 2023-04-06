784dd6fd460f5b400fde23470fee4cb6fff3061b
Jacqueline Alves <jacque.alves.b@gmail.com>
2023-04-06 08:08:30 -0300

    refactors task 01

--- task 01/rpnStacker.py
+++ task 01/rpnStacker.py
@@ -1,40 +1,47 @@
 import sys
 
+class RPNStacker:
+
+    operations = {
+        "+": lambda x, y : x + y,
+        "-": lambda x, y : x - y,
+        "*": lambda x, y : x * y,
+        "/": lambda x, y : x / y
+    }    
+
+    def getLines(self, inputFile):    
+        file = open(inputFile, "r")
+        return file.readlines()
+
+    def eval(self, inputFile):
+        lines = self.getLines(inputFile)
+
+        i = 0
+        first, second, op = None, None, None
+        while i < len(lines):
+            line = str.strip(lines[i])
+
+            if first == None:
+                first = int(line)
+                i += 1
+                
+            elif second == None:
+                second = int(line)
+                i += 1
+                
+            elif op == None:
+                op = line
+                i += 1
+            
+            if first and second and op:
+                fn = self.operations[op]
+                first = fn(first, second)
+                second = None
+                op = None
+
+        return first
+
+rpnStacker = RPNStacker()
 inputFile = sys.argv[1] if len(sys.argv) > 1 else "Calc1.stk"
-file = open(inputFile, "r")
-lines = file.readlines()
-
-def sum(x, y): return x + y
-def sub(x, y): return x - y
-def mul(x, y): return x * y
-def div(x, y): return x / y
-
-operations = {
-    "+": sum,
-    "-": sub,
-    "*": mul,
-    "/": div
-}
- 
-i = 0
-first, second, op = None, None, None
-while i < len(lines):
-    line = lines[i]
-
-    if first == None:
-        first = int(str.strip(line))
-        i += 1
-    elif second == None:
-        second = int(str.strip(line))
-        i += 1
-    elif op == None:
-        op = str.strip(line)
-        i += 1
-    
-    if first and second and op:
-        fn = operations[op]
-        first = fn(first, second)
-        second = None
-        op = None
-
-print(first)
+out = rpnStacker.eval(inputFile)
+print(out)
